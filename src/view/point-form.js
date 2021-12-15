47502a5ab980baed997a85f432a6478ce3880fb9
alexeyzuby@bk.ru <6978757Eee>
2021-12-15 19:12:38 +0300

    Добавил пропущенный класс для элемента списка

--- src/view/point-form.js
+++ src/view/point-form.js
@@ -91,7 +91,7 @@ const createPointFormTemplate = (points = {}) => {
   const endTime = dayjs(dateTo).format('DD/MM/YY HH:mm');
 
   return (
-    `<li>
+    `<li class="trip-events__item">
        <form class="event event--edit" action="#" method="post">
          <header class="event__header">
            <div class="event__type-wrapper">
