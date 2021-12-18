07b909654c9563265950c2426030133d8aab2867
alexeyzuby@bk.ru <6978757Eee>
2021-12-19 00:42:13 +0300

    Добавил новые обрабочики для закрытия формы и отображения сообщения, если точек нет

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import SortView from './view/sort';
 import PointsListView from './view/points-list';
 import PointFormView from './view/point-form';
 import PointView from './view/point';
+import NoPointsView from './view/no-points';
 import {render, RenderPosition} from './render';
 import {generatePoint} from './mock/point';
 
-const POINTS_COUNT = 15;
+const POINTS_COUNT = 10;
 
 const header = document.querySelector('.page-header');
 const pageMain = document.querySelector('.page-main');
@@ -37,20 +38,39 @@ const renderPoint = (pointsListElement, task) => {
     pointsListElement.replaceChild(pointComponent.element, pointFormComponent.element);
   };
 
+  const onEscKeyDown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      replaceFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
   pointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
     replacePointToForm();
+    document.addEventListener('keydown', onEscKeyDown);
   });
 
   pointFormComponent.element.querySelector('form').addEventListener('submit', (evt) => {
     evt.preventDefault();
     replaceFormToPoint();
+    document.addEventListener('keydown', onEscKeyDown);
+  });
+
+  pointFormComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+    replaceFormToPoint();
+    document.addEventListener('keydown', onEscKeyDown);
   });
 
   render(pointsListElement, pointComponent.element, RenderPosition.BEFOREEND);
 };
 
-render(tripEvents, pointsListComponent.element, RenderPosition.BEFOREEND);
+if (points.length) {
+  render(tripEvents, pointsListComponent.element, RenderPosition.BEFOREEND);
 
-for (let i = 0; i < POINTS_COUNT; i++) {
-  renderPoint(pointsListComponent.element, points[i]);
+  for (let i = 0; i < POINTS_COUNT; i++) {
+    renderPoint(pointsListComponent.element, points[i]);
+  }
+} else {
+  render(tripEvents, new NoPointsView().element, RenderPosition.BEFOREEND);
 }
